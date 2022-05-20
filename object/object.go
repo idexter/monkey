0c1beddfc7337d4116cb77e0f402705a1892e177
Anton Kucherov <idexter.ru@gmail.com>
2022-05-20 23:55:51 +0300

    Add basic objects for implementing REPL.

---
+++ object/object.go
@@ -0,0 +1,35 @@
+package object
+
+import "fmt"
+
+type Type string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+type Object interface {
+	Type() Type
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() Type      { return INTEGER_OBJ }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() Type      { return BOOLEAN_OBJ }
+
+type Null struct{}
+
+func (n *Null) Inspect() string { return "null" }
+func (n *Null) Type() Type      { return NULL_OBJ }
