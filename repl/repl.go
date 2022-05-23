9f48d21e1224d8347f41015b8c4547b125995c89
Anton Kucherov <idexter.ru@gmail.com>
2022-05-23 19:52:19 +0300

    Add identifiers and variables to evaluator.

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/idexter/monkey/evaluator"
 	"github.com/idexter/monkey/lexer"
+	"github.com/idexter/monkey/object"
 	"github.com/idexter/monkey/parser"
 )
 
@@ -15,6 +16,7 @@ const PROMPT = ">> "
 // StartREPL implements Read-Parse-Print-Loop.
 func StartREPL(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -33,7 +35,7 @@ func StartREPL(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
