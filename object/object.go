466b4bd1dabc8488f56f5c6fb78d86103f6032cb
Anton Kucherov <idexter.ru@gmail.com>
2022-05-24 22:53:06 +0300

    Implement arrays in evaluator.

--- object/object.go
+++ object/object.go
@@ -19,6 +19,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
 
 type Object interface {
@@ -98,3 +99,21 @@ type Builtin struct {
 
 func (b *Builtin) Type() Type      { return BUILTIN_OBJ }
 func (b *Builtin) Inspect() string { return "builtin function" }
+
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() Type { return ARRAY_OBJ }
+
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+	elements := []string{}
+	for _, e := range ao.Elements {
+		elements = append(elements, e.Inspect())
+	}
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+	return out.String()
+}
